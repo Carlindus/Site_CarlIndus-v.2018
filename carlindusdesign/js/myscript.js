31fbf5d2d801ece24cb33fa9a71092d5ee37a7ca
carlindus <carlindusdesign@gmail.com>
2018-11-22 12:51:38 +0100

    add files to the project already done

---
+++ carlindusdesign/js/myscript.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+
+  $('#contactCID-ico').click(function(){
+    $('#contactCID').show();
+  });
+
+  $('#contactCID .cross').click(function(){
+   $('#contactCID').hide();
+  });
+
+  $('#website-manager-ico').click(function(){
+    $('#website-manager').show();
+  });
+
+  $('#website-manager .cross').click(function(){
+   $('#website-manager').hide();
+  });
+
+  $('#recycle-bin-ico').click(function(){
+    $('#recycle-bin').show();
+  });
+
+  $('#recycle-bin .cross').click(function(){
+   $('#recycle-bin').hide();
+ });
+
+
+});
