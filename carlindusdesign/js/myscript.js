31c87e48a76eda8c36eb437c5e47c7dee513469b
carlindus <carlindusdesign@gmail.com>
2018-11-24 10:50:04 +0100

    Update contact section Add validform.js Active contact with /phpPart

--- carlindusdesign/js/myscript.js
+++ carlindusdesign/js/myscript.js
@@ -1,40 +1,40 @@
 $(document).ready(function() {
 
-$('article').click(function(){
-  $(this).focus();
+  // $('article').click(function() {
+  //   $(this).focus();
+  // });
+
+  $('#contactForm-ico').click(function() {
+    $('#contactForm').show();
+  });
+
+  $('#contactForm .cross').click(function() {
+    $('#contactForm').hide();
+  });
+
+
+  $('#contactCID-ico').click(function() {
+    $('#contactCID').show();
+  });
+
+  $('#contactCID .cross').click(function() {
+    $('#contactCID').hide();
+  });
+
+  $('#website-manager-ico').click(function() {
+    $('#website-manager').show();
+  });
+
+  $('#website-manager .cross').click(function() {
+    $('#website-manager').hide();
+  });
+
+  $('#recycle-bin-ico').click(function() {
+    $('#recycle-bin').show();
+  });
+
+  $('#recycle-bin .cross').click(function() {
+    $('#recycle-bin').hide();
+  });
+
 });
-
-      $('#contactForm-ico').click(function() {
-        $('#contactForm').show();
-      });
-
-      $('#contactForm .cross').click(function() {
-        $('#contactForm').hide();
-      });
-
-
-      $('#contactCID-ico').click(function() {
-        $('#contactCID').show();
-      });
-
-      $('#contactCID .cross').click(function() {
-        $('#contactCID').hide();
-      });
-
-      $('#website-manager-ico').click(function() {
-        $('#website-manager').show();
-      });
-
-      $('#website-manager .cross').click(function() {
-        $('#website-manager').hide();
-      });
-
-      $('#recycle-bin-ico').click(function() {
-        $('#recycle-bin').show();
-      });
-
-      $('#recycle-bin .cross').click(function() {
-        $('#recycle-bin').hide();
-      });
-
-    });
